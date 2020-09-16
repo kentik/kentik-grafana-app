e0cfd3ea7bbaaa46806f39816f4ed17394a5141a
rozetko <rozetko@corpglory.com>
2020-09-16 17:23:09 +0300

    do not show alerts when Member tries to query custom dimensions

--- src/datasource/kentik_api.ts
+++ src/datasource/kentik_api.ts
@@ -32,9 +32,17 @@ export class KentikAPI {
     return this.invokeSQLQuery(query);
   }
 
-  async getCustomDimensions(): Promise<any> {
-    const data = await this._get('/api/v5/customdimensions');
-    return data.data.customDimensions;
+  async getCustomDimensions(): Promise<any[]> {
+    try {
+      const silent = true;
+      const resp = await this._get('/api/v5/customdimensions', silent);
+      return resp.data.customDimensions;
+    } catch (e) {
+      if (e.status === 403) {
+        return [];
+      }
+      throw e;
+    }
   }
 
   async getSavedFilters(): Promise<any> {
@@ -58,7 +66,7 @@ export class KentikAPI {
     return this._post('/api/v5/query/sql', data);
   }
 
-  private async _get(url: string): Promise<any> {
+  private async _get(url: string, silent = false): Promise<any> {
     try {
       const resp = await this.$http({
         method: 'GET',
@@ -67,7 +75,9 @@ export class KentikAPI {
 
       return resp;
     } catch (error) {
-      showAlert(error);
+      if (silent === false) {
+        showAlert(error);
+      }
       if (error.err) {
         throw error.err;
       } else {
