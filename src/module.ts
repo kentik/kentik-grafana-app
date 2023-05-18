1589c8b5e58d621b92abec1491408adb0f9e5a9d
rozetko <rozetko@corpglory.com>
2023-05-18 18:17:27 +0300

    migrate Config page from Angular to React

--- src/module.ts
+++ src/module.ts
@@ -1,10 +1,10 @@
-import { AppPlugin } from '@grafana/data';
-import { RootPage } from './RootPage';
+import { RootPage } from './components/RootPage';
+import { AppConfig } from './components/AppConfig';
 
 import './styles/dark.scss';
 import './styles/light.scss';
 
-import { ConfigCtrl } from './config/config';
+import { AppPlugin } from '@grafana/data';
 import { loadPluginCss } from 'grafana/app/plugins/sdk';
 
 loadPluginCss({
@@ -12,6 +12,9 @@ loadPluginCss({
   light: 'plugins/kentik-connect-app/styles/light.css',
 });
 
-export { ConfigCtrl };
-
-export const plugin = new AppPlugin<{}>().setRootPage(RootPage);
+export const plugin = new AppPlugin<{}>().setRootPage(RootPage).addConfigPage({
+  title: 'Configuration',
+  icon: 'cog',
+  body: AppConfig,
+  id: 'configuration',
+});;
