e8b242124150bfb92d3d4c950e0cb805b549c3bb
rozetko <rozetko@corpglory.com>
2023-05-24 20:52:05 +0300

    rename Description component to DescriptionPanel

--- src/panel/description/module.ts
+++ src/panel/description/module.ts
@@ -1,4 +1,4 @@
 import { PanelPlugin } from '@grafana/data';
-import { DescriptionPanel } from '../../components/Description';
+import { DescriptionPanel } from '../../components/DescriptionPanel';
 
 export const plugin = new PanelPlugin<{}>(DescriptionPanel);
