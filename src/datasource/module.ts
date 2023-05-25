a003a03cf7aa59e217ff6f2c23a688b2285c35c3
rozetko <rozetko@corpglory.com>
2023-05-25 18:35:20 +0300

    fix import order

--- src/datasource/module.ts
+++ src/datasource/module.ts
@@ -1,7 +1,7 @@
 import { KentikDataSource, KentikQuery } from './new_datasource';
-import { DataSourcePlugin } from '@grafana/data';
-
 import { QueryEditor } from './query_editor/QueryEditor';
+
+import { DataSourcePlugin } from '@grafana/data';
 
 export const plugin = new DataSourcePlugin<KentikDataSource, KentikQuery, {}>(KentikDataSource).setQueryEditor(
   QueryEditor
--- src/panel/description/module.ts
+++ src/panel/description/module.ts
@@ -1,4 +1,5 @@
-import { PanelPlugin } from '@grafana/data';
 import { DescriptionPanel } from '../../components/DescriptionPanel';
+
+import { PanelPlugin } from '@grafana/data';
 
 export const plugin = new PanelPlugin<{}>(DescriptionPanel);
