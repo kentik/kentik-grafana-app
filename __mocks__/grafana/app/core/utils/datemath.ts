f83c812568779ef8c555e5dd7af9b6cdd80b859c
Joanna Stawiarska <joannastawiarska@ST11372-joasta.local>
2025-10-24 10:56:05 +0200

    Created a new plugin

--- __mocks__/grafana/app/core/utils/datemath.ts
+++
@@ -1,3 +0,0 @@
-export default {
-  parse: jest.fn()
-};
--- src/datasource/module.ts
+++
@@ -1,9 +0,0 @@
-import { ConfigEditor } from './ConfigEditor';
-import { KentikDataSource, KentikQuery } from './datasource';
-import { QueryEditor } from './QueryEditor';
-
-import { DataSourcePlugin } from '@grafana/data';
-
-export const plugin = new DataSourcePlugin<KentikDataSource, KentikQuery, {}>(KentikDataSource)
-  .setConfigEditor(ConfigEditor)
-  .setQueryEditor(QueryEditor);
